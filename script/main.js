cbf505d6858d4edaf075b2b5e0392dc5c9090ca1
cedrus172 <64965926+cedrus172@users.noreply.github.com>
2021-09-21 21:17:28 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,16 +1,32 @@
 function convert() {
     let amount = parseFloat(document.getElementById('amountTxt').value);
+
     let fromCurrency = document.getElementById('fromCurrency').value;
     let toCurrency = document.getElementById('toCurrency').value;
+
     let result = document.getElementById('result');
+
     let toCurValue = 0;
-    switch (toCurrency) {
+    let fromCurValue = 0;
+
+    switch (fromCurrency) { // lấy giá trị from Currency Value
+        case "vnd":
+            fromCurValue = 1;
+            break;
+        case "usd":
+            fromCurValue = 23000;
+            break;
+    }
+    
+    switch (toCurrency) { // lấy giá trị to Currency Value
         case "vnd":
             toCurValue = 1;
             break;
         case "usd":
-            toCurValue = 23;
+            toCurValue = 23000;
             break;
     }
-    result.innerText = amount * toCurValue;
+
+    result.innerText = (amount * fromCurValue / toCurValue).toFixed(2);
+       
 }
